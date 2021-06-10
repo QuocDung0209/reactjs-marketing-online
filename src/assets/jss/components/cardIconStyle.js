d9e93ec234d17a2beadcecbf4184cd5c1ead7595
QuocDung0209 <quocdung02091997@gmail.com>
2021-06-10 17:56:40 +0700

    [Core Components] Add Card components

---
+++ src/assets/jss/components/cardIconStyle.js
@@ -0,0 +1,30 @@
+import {
+  warningCardHeader,
+  successCardHeader,
+  dangerCardHeader,
+  infoCardHeader,
+  primaryCardHeader,
+  roseCardHeader,
+  grayColor
+} from "./cardBase";
+
+const cardIconStyle = {
+  cardIcon: {
+    "&$warningCardHeader,&$successCardHeader,&$dangerCardHeader,&$infoCardHeader,&$primaryCardHeader,&$roseCardHeader": {
+      borderRadius: "3px",
+      backgroundColor: grayColor,
+      padding: "15px",
+      marginTop: "-20px",
+      marginRight: "15px",
+      float: "left"
+    }
+  },
+  warningCardHeader,
+  successCardHeader,
+  dangerCardHeader,
+  infoCardHeader,
+  primaryCardHeader,
+  roseCardHeader
+};
+
+export default cardIconStyle;
