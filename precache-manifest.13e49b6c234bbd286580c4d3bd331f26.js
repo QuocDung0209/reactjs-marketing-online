2d9dee8dd1b449698e836459543bb8e77a8d4315
QuocDung0209 <quocdung02091997@gmail.com>
2021-06-18 16:11:53 +0700

    Updates

---
+++ precache-manifest.13e49b6c234bbd286580c4d3bd331f26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e644219646d636d4368aa8d424040274",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d7207fa10e1d52214eb",
+    "url": "/static/css/main.4648553e.chunk.css"
+  },
+  {
+    "revision": "e97cfe7825b5170383a7",
+    "url": "/static/js/2.2bbc3d8b.chunk.js"
+  },
+  {
+    "revision": "2ce22801400a709a31b4255d4bfc662a",
+    "url": "/static/js/2.2bbc3d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d7207fa10e1d52214eb",
+    "url": "/static/js/main.e3fcd8d2.chunk.js"
+  },
+  {
+    "revision": "2e79a20fe3113fc70868",
+    "url": "/static/js/runtime-main.9c034610.js"
+  },
+  {
+    "revision": "2b737b6eed55391a57ea734cf87552df",
+    "url": "/static/media/loading.2b737b6e.gif"
+  }
+]);
