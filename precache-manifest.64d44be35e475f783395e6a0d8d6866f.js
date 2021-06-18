8b79ac8182967155072ce22532d19c6b227594a8
QuocDung0209 <quocdung02091997@gmail.com>
2021-06-18 16:28:44 +0700

    Updates

---
+++ precache-manifest.64d44be35e475f783395e6a0d8d6866f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a96f085cc62351e56ae6c7186ce5993",
+    "url": "/marketing-online/index.html"
+  },
+  {
+    "revision": "212d3a1336269784788a",
+    "url": "/marketing-online/static/css/main.4648553e.chunk.css"
+  },
+  {
+    "revision": "7ce81d945699ef03a2c0",
+    "url": "/marketing-online/static/js/2.22edb9d6.chunk.js"
+  },
+  {
+    "revision": "2ce22801400a709a31b4255d4bfc662a",
+    "url": "/marketing-online/static/js/2.22edb9d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "212d3a1336269784788a",
+    "url": "/marketing-online/static/js/main.0134b3aa.chunk.js"
+  },
+  {
+    "revision": "8b15d3300bb6480bc21f",
+    "url": "/marketing-online/static/js/runtime-main.61957ce7.js"
+  },
+  {
+    "revision": "2b737b6eed55391a57ea734cf87552df",
+    "url": "/marketing-online/static/media/loading.2b737b6e.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.13e49b6c234bbd286580c4d3bd331f26.js"
+  "/marketing-online/precache-manifest.64d44be35e475f783395e6a0d8d6866f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/marketing-online/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
